2c35407b3fc2780e2abae4f3668d113e9310013b
Rrrafa <rrrafa@Rrrafas-iMac.fios-router.home>
2020-12-10 00:58:47 -0500

    cleanup

--- Assets/Resources/Scripts/Player/PlayerController.cs
+++ Assets/Resources/Scripts/Player/PlayerController.cs
@@ -23,12 +23,13 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * speed * Time.fixedDeltaTime;
+        if (WidgetManager.singleton.isState(WidgetManager.MERCHANT_STATE))
+        {
+            return;
+        }
         Vector2 target = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * speed * Time.fixedDeltaTime;
         Vector2 vel = Vector2.zero;
         GetComponent<Rigidbody2D>().velocity = Vector2.SmoothDamp(GetComponent<Rigidbody2D>().velocity, target, ref vel, 0.025f); ;
-
-            //(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * speed * Time.fixedDeltaTime;
 
     }
 }
